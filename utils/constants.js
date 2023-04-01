0f624f712da794674f43d7350d99d38149f43c98
Alexandr Ukhanov <ukhanov1996@yandex.ru>
2023-04-01 22:03:54 +0700

    change PORT on 3001

--- utils/constants.js
+++ utils/constants.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 module.exports.corsOptions = {
   origin: [
-    // 'https://movies.ukh.nomoredomains.work',
-    // 'http://movies.ukh.nomoredomains.work',
+    'https://movies.ukh.nomoredomains.work',
+    'http://movies.ukh.nomoredomains.work',
     'http://localhost:3001',
   ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -15,5 +15,5 @@ module.exports.rateLimitOptions = {
   max: 100,
 };
 
-const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/moviesdblocal' } = process.env;
+const { PORT = 3001, DB_URL = 'mongodb://localhost:27017/moviesdblocal' } = process.env;
 module.exports = { PORT, DB_URL };
